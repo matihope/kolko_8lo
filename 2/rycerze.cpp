0249d6209a8a79cbc7d217954aed369ca370e382
Mateusz Kolpa <mateusz.kolpa@wp.pl>
2020-02-10 20:14:51 +0100

    Lil code optimizations

--- 2/rycerze.cpp
+++ 2/rycerze.cpp
@@ -4,18 +4,17 @@ using namespace std;
 
 int main(){
     ios::sync_with_stdio(false);
-
+    
     int t; cin >> t;
 
-    vector<int> szczyty;
-    for(int i = 0; i < t; i++){
-        int a = 0; cin >> a;
-        szczyty.push_back(a);
-    }
+    int sz[t];
+    for(int i = 0; i < t; i++) cin >> sz[i];
 
     int count = 0;
-    for(int i = 1; i < t-1; i++){
-        if ((szczyty[i-1] < szczyty[i]) && (szczyty[i] > szczyty[i+1])){
+    for(int i = 1; i < t-1; i++)
+    {
+        if ((sz[i-1] < sz[i]) && (sz[i] > sz[i+1]))
+        {
             count++;
         }
     }
