b1d9265e26c9477ae33b0a7a7b4f4161ea01cdf4
Mateusz Kolpa <mateusz.kolpa@wp.pl>
2020-02-14 20:59:44 +0100

    2/trl.cpp is working now -.- Thanks to @https://github.com/Al-Gor1thm

--- 2/trl.cpp
+++ 2/trl.cpp
@@ -1,113 +1,142 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 
+// using namespace std;
+
+// bool jest_prostokatny(vector<double> trojkat){
+//     return (pow(trojkat[0], 2) + pow(trojkat[1], 2) == pow(trojkat[2], 2));
+// }
+
+// double licz_pole(vector<double>& t){
+//     double p = (t[0] + t[1] + t[2]) * 0.5;
+//     cout << p << endl;
+//     return sqrt(p*(p-t[0])*(p-t[1])*(p-t[2]));
+// }
+
+// bool czy_pole(vector<double> trojkat, double last_pole){
+//     double n_pole = licz_pole(trojkat);
+//     return ((last_pole*0.8 <= n_pole) && (n_pole <= last_pole*1.2));
+// }
+
+// void inpt(vector<double>& trojkat){
+//     trojkat.clear();
+//     for(int i = 0; i < 3; i++){
+//         double a; cin >> a;
+//         trojkat.push_back(a);
+//     }
+//     sort(trojkat.begin(), trojkat.end());
+// }
+
+// int main(){
+//     int punkty_pani = 0, punkty_pana = 0;
+
+//     vector<double> trojkat;
+//     double pole = 0;
+
+//     bool
+//     pole_sie_zgadza = false,
+//     prostokatny = false,
+
+//     ruch_pani = false;
+
+//     do{
+//         // input and test if it's a triangle
+//         inpt(trojkat);
+//         ruch_pani = !ruch_pani;
+//         if((trojkat[0] + trojkat[1]) <= trojkat[2]) continue;
+
+//         // points
+//         if(ruch_pani) punkty_pani++;
+//         else punkty_pana++;
+
+//         if(czy_pole(trojkat, pole)){
+//             punkty_pani++;
+//             if(jest_prostokatny(trojkat))
+//                 punkty_pani++;
+//         }
+//         pole = licz_pole(trojkat);
+
+//     } while (trojkat[0] != 0 && trojkat[1] != 0 && trojkat[2] != 0);
+
+//     cout << punkty_pani << " " << punkty_pana << endl;
+
+//     return 0;
+// }
+
+// THE UPPER CODE SOMEHOW DOESN'T WORK :/ NO CLUE WHY
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
-
-bool czy_jest_trojkatem(vector<double> trojkat){
-    if(trojkat[0] + trojkat[1] > trojkat[2]) 
-        return true;
-
-    return false;
-}
-
-bool czy_jest_prostokatny(vector<double> trojkat){
-    if(pow(trojkat[0], 2) + pow(trojkat[1], 2) == pow(trojkat[2], 2))
-        return true;
-    
-    return false;
-}
-
-
-bool czy_pole(vector<double> t1, vector<double> t2){
-    // t1 = nowe, t2 = stare
-    double p1 = (double)(t1[0] + t1[1] + t1[2]) / 2;
-    double pole1 = (sqrt(p1*(p1-t1[0])*(p1-t1[1])*(p1-t1[2])));
-
-    double p2 = (double)(t2[0] + t2[1] + t2[2]) / 2;
-    double pole2 = (sqrt(p2*(p2-t2[0])*(p2-t2[1])*(p2-t2[2])));
-
-    if((pole2*(80.0/100.0) <= pole1) && (pole1 <= pole2*(120.0/100.0))){
-        return true;
-    }
-
-    return false;
-}
-
-
-void pani_inpt(vector<double>& pani){
-    pani.erase(pani.begin(), pani.end());
-    for(int i = 0; i < 3; i++){
-        double a; cin >> a; 
-        pani.push_back(a);
-    }
-    sort(pani.begin(), pani.end());
-}
-
-
-void pan_inpt(vector<double>& pan){
-    pan.erase(pan.begin(), pan.end());
-    for(int i = 0; i < 3; i++){
-        double a; cin >> a; 
-        pan.push_back(a);
-    }
-    sort(pan.begin(), pan.end());
-}
-
-
-int main(){
-    vector<double> pani;
-    vector<double> pan;
-
-    int punkty_pani = 0, punkty_pana = 0;
-
-    pani_inpt(pani);
-
-    bool 
-    poprzedni_byl_trojkatem = false,
-    jest_trojkatem = czy_jest_trojkatem(pani), 
-    pole = false, 
-    prostokatny = czy_jest_prostokatny(pani);
-
-    do{
-        if(jest_trojkatem)
+int main()
+{
+    int in, fp = 0, mp = 0;
+    bool who = true; //T => m
+    short test;
+    double lastarea, area, p, a, b, c;
+    area = 0;
+    vector<int> sides;
+    while (1)
+    {
+        lastarea = area;
+        test = 0;
+        sides.clear();
+        who = !who; //reset lines
+        for (int i = 0; i < 3; i++)
         {
-            punkty_pani++;
-            if(pole && poprzedni_byl_trojkatem)
+            cin >> in;
+            if (in == 0)
             {
-                punkty_pani++;
-                if(prostokatny) 
-                    punkty_pani++;
+                test++;
+            } //input
+            sides.push_back(in);
+        }
+        if (test == 3)
+        {
+            break;
+        }                                 //3x0?
+        sort(sides.begin(), sides.end()); //sort
+        if ((sides[0] + sides[1]) <= sides[2])
+        {
+            continue;
+        } //is it a triangle?
+        
+        if (who == false)
+        {
+            fp++;
+        }
+        else
+        {
+            mp++;
+        } //+1 for the person
+        a = sides[0];
+        b = sides[1];
+        c = sides[2];
+        p = (a + b + c) * 0.5;
+        area = sqrt(p * (p - a) * (p - b) * (p - c));
+        if ((area >= lastarea * 0.8) && (area <= lastarea * 1.2))
+        {
+            if (who == false)
+            {
+                fp++;
+            }
+            else
+            {
+                mp++;
+            }
+            if (a * a + b * b == c * c)
+            {
+                if (who == false)
+                {
+                    fp++;
+                }
+                else
+                {
+                    mp++;
+                }
             }
         }
-
-        pan_inpt(pan);
-        poprzedni_byl_trojkatem = jest_trojkatem;
-        jest_trojkatem = czy_jest_trojkatem(pan);
-        pole = czy_pole(pan, pani);
-        prostokatny = czy_jest_prostokatny(pan);
-
-        if(jest_trojkatem)
-        {
-            punkty_pana++;
-            if(pole && poprzedni_byl_trojkatem)
-            {
-                punkty_pana++;
-                if(prostokatny) 
-                    punkty_pana++;
-            }
-        }
-
-        //pani
-        pani_inpt(pani);
-        poprzedni_byl_trojkatem = jest_trojkatem;
-        jest_trojkatem = czy_jest_trojkatem(pani);
-        pole = czy_pole(pani, pan);
-        prostokatny = czy_jest_prostokatny(pani);
-
-    }while(pani[0] != 0 && pan[1] != 0);
-
-    cout << punkty_pani << " " << punkty_pana << endl;
-
+    }
+    cout << fp << ' ' << mp;
     return 0;
 }
