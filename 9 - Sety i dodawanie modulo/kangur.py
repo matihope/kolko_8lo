14afcd7fb560e753a37494a61c83e3ae71e6c431
Mateusz Kolpa <mateusz.kolpa@wp.pl>
2020-06-01 11:31:54 +0200

    Update 01.06.2020

--- 9 - Sety i dodawanie modulo/kangur.py
+++
@@ -1,6 +0,0 @@
-import math
-
-for i in range(2019, 2100):
-    thing = math.sqrt(int(str(i)[:2]) * int(str(i)[2:]))
-    if round(thing) == thing:
-        print(i)
--- 9 - Sety i dodawanie modulo/potega.py
+++
@@ -1,5 +0,0 @@
-import math
-
-testcase = int(input(':'))
-testcase = math.factorial(testcase)
-print(testcase % (1e9+7))
